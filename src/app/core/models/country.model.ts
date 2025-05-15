cf1e6388b196224b281c7472dd75723d0b1c86cd
Alvaro Gomez <alvaro@supermark.co>
2025-05-15 01:35:18 -0500

    country model created

---
+++ src/app/core/models/country.model.ts
@@ -0,0 +1,83 @@
+export interface Country {
+  name: {
+    common: string;
+    official: string;
+    nativeName?: {
+      [lang: string]: {
+        official: string;
+        common: string;
+      };
+    };
+  };
+  tld?: string[];
+  cca2: string;
+  ccn3?: string;
+  cca3: string;
+  cioc?: string;
+  independent?: boolean;
+  status?: string;
+  unMember?: boolean;
+  currencies?: {
+    [code: string]: {
+      name: string;
+      symbol: string;
+    };
+  };
+  idd?: {
+    root?: string;
+    suffixes?: string[];
+  };
+  capital?: string[];
+  altSpellings?: string[];
+  region: string;
+  subregion?: string;
+  languages?: {
+    [code: string]: string;
+  };
+  latlng?: number[];
+  landlocked?: boolean;
+  borders?: string[];
+  area?: number;
+  demonyms?: {
+    [lang: string]: {
+      f: string;
+      m: string;
+    };
+  };
+  translations?: {
+    [lang: string]: {
+      official: string;
+      common: string;
+    };
+  };
+  flag?: string;
+  flags?: {
+    png: string;
+    svg: string;
+    alt?: string;
+  };
+  maps?: {
+    googleMaps: string;
+    openStreetMaps: string;
+  };
+  population?: number;
+  fifa?: string;
+  car?: {
+    signs?: string[];
+    side?: 'left' | 'right';
+  };
+  timezones?: string[];
+  continents?: string[];
+  coatOfArms?: {
+    png?: string;
+    svg?: string;
+  };
+  startOfWeek?: string;
+  capitalInfo?: {
+    latlng?: number[];
+  };
+  postalCode?: {
+    format?: string | null;
+    regex?: string | null;
+  };
+}
--- src/app/core/services/country.service.ts
+++ src/app/core/services/country.service.ts
@@ -1,9 +1,12 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountryService {
 
-  constructor() { }
+  private readonly http = inject(HttpClient);
+
+  private readonly baseUrl = 'https://restcountries.com/v3.1';
 }
