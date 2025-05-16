cc18eaab910d5ad4b47c58466ac24d9392de0df2
Alvaro Gomez <alvaro@supermark.co>
2025-05-16 01:24:26 -0500

    mat icons added to country card and header components

--- src/app/features/components/country-card/country-card.component.ts
+++ src/app/features/components/country-card/country-card.component.ts
@@ -1,11 +1,14 @@
 import { ChangeDetectionStrategy, Component, computed, inject, input } from '@angular/core';
+
+import { MatIconModule } from '@angular/material/icon';
+
 import { Country } from '../../../core/models/country.model';
 import { FavoritesStore } from '../../../core/store/favorites.store';
 import { FormatNumberPipe } from '../../../core/pipes/format-number.pipe';
 
 @Component({
   selector: 'app-country-card',
-  imports: [FormatNumberPipe],
+  imports: [FormatNumberPipe, MatIconModule],
   templateUrl: './country-card.component.html',
   styleUrl: './country-card.component.scss',
   providers: [],
